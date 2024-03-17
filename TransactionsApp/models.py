12f9e614f0e5a458cacf4df2ff4ed85707328b3e
JustUsNow <79359475+JustUsNow@users.noreply.github.com>
2024-03-18 03:10:34 +0700

    Модель транзакции
    Создал простую модель транзакции.

--- TransactionsApp/models.py
+++ TransactionsApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Transaction(models.Model):
+    OwnerId = models.ForeignKey('auth.User', related_name='transactions', on_delete=models.CASCADE)
+    transactionDate = models.DateTimeField()
+    transactionType = models.CharField(max_length=100)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return f'Owner: {self.transactionOwnerId}, \
+            Date: {self.transactionDate}, \
+            Type: {self.transactionType}, \
+            Amount: {self.amount}'
