65de939480d64ae2ad47b83b16a0ca5c6813ae7a
JustUsNow <79359475+JustUsNow@users.noreply.github.com>
2024-03-18 06:46:40 +0700

    Конечные точки API

--- TransactionsApp/views.py
+++ TransactionsApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Transaction
+from .serializers import TransactionSerializer
 
 # Create your views here.
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated,]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(ownerId=user)
--- UsersApp/views.py
+++ UsersApp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated,]
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(id=user.id)
+
