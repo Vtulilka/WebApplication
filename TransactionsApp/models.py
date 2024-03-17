801842b8547313af6fe9b8fc48275042296993f7
JustUsNow <79359475+JustUsNow@users.noreply.github.com>
2024-03-18 06:44:45 +0700

    Миграция
    Изменил названия некоторых полей

--- TransactionsApp/models.py
+++ TransactionsApp/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Transaction(models.Model):
-    OwnerId = models.ForeignKey('auth.User', related_name='transactions', on_delete=models.CASCADE)
-    transactionDate = models.DateTimeField()
-    transactionType = models.CharField(max_length=100)
+    ownerId = models.ForeignKey('auth.User', related_name='transactions', on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    type = models.CharField(max_length=100)
     amount = models.IntegerField()
 
     def __str__(self):
-        return f'Owner: {self.transactionOwnerId}, \
-            Date: {self.transactionDate}, \
-            Type: {self.transactionType}, \
+        return f'Owner: {self.OwnerId}, \
+            Date: {self.Date}, \
+            Type: {self.Type}, \
             Amount: {self.amount}'
