f09ee397e91b0dca6039abbbf5d699860885ecc5
JustUsNow <79359475+JustUsNow@users.noreply.github.com>
2024-03-20 11:05:08 +0700

    Конечные точки для управления пользователями

--- UsersApp/serializers.py
+++ UsersApp/serializers.py
@@ -1,9 +1,86 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from TransactionsApp.serializers import TransactionSerializer
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 
 
 class UserSerializer(serializers.ModelSerializer):  
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', ]
+        extra_kwargs = { 
+            'password': { 'write_only': True },
+            'id': { 'read_only': True },
+            }
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        label='Username', 
+        write_only=True
+    )
+    password = serializers.CharField(
+        label='Password',
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+        
+        # Проверка имени пользователя и пароля
+        if username and password:
+            if '@' in username:
+                try:
+                    user = User.objects.get(email=username)
+                except ObjectDoesNotExist:
+                    pass
+            else:
+                user = authenticate(request=self.context['request'],
+                        username=username, 
+                        password=password)
+
+            # Пользователь не прошёл аутентификацию
+            if not user:
+                msg = f'Access denied: wrong username or password. {password}'
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            # В запросе не переданы данные для аутентификации
+            msg = 'Both "username" and "password" are required.'
+            raise serializers.ValidationError(msg, code='authorization')
+        
+        attrs['user'] = user
+        return attrs
+    
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', ]
+        extra_kwargs = { 
+            'password': { 'write_only': True },
+            'id': { 'read_only': True },
+            }
+        
+    def validate(self, attrs):
+        email_exists = User.objects.filter(email=attrs['email']).exists()
+        if email_exists:
+            raise ValidationError('Error: email already exists')
+        
+        username_exists = User.objects.filter(username=attrs['username']).exists()
+        if username_exists:
+            raise ValidationError('Error: username already exists')
+        
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
--- UsersApp/views.py
+++ UsersApp/views.py
@@ -1,14 +1,50 @@
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import mixins, viewsets, permissions, status
+from rest_framework.decorators import action, permission_classes
+from rest_framework.response import Response
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from django.contrib.auth import login, logout
+from . import serializers
 
-# Create your views here.
+
 class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated,]
+    serializer_class = serializers.UserSerializer
+    permission_classes=[permissions.IsAuthenticated, ]
+    permission_classes_by_action = {'create': [permissions.AllowAny],
+                                    'login': [permissions.AllowAny]}
 
+    # Получение данных только о текущем пользователе
     def get_queryset(self):
         user = self.request.user
         return User.objects.filter(id=user.id)
 
+    def get_permissions(self):
+        try: 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
+        
+    def create(self, request):
+        data = request.data
+        serializer = serializers.SignUpSerializer(data=data)
+        
+        if serializer.is_valid(raise_exceptions=True):
+            serializer.save()
+        
+        return Response(serializer.data)
+
+    @action(detail=False, 
+            methods=['post'],
+            permission_classes=[permissions.AllowAny, ])
+    def login(self, request):
+        serializer = serializers.LoginSerializer(data=self.request.data,
+            context={ 'request': self.request })
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response(None, status=status.HTTP_202_ACCEPTED)
+    
+    @action(detail=False, 
+            methods=['get'])
+    def logout(self, request):
+        logout(request)
+        return Response(None, status=status.HTTP_200_OK)
